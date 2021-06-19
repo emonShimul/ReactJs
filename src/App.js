dcd84b14f2541df12cb0b2abaea0a7d5bec1f32c
emoncse53 <emoncse53@gmail.com>
2021-06-19 23:51:34 +0600

    Format the document

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
-  const [users,setUsers]=useState([])
-  const [name,setName]=useState("")
-  const [username,setUsername]=useState("")
-  const [email,setEmail]=useState("")
-  const [phone,setPhone]=useState("")
-  const [id,setId]=useState(null)
+  const [users, setUsers] = useState([])
+  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [phone, setPhone] = useState("")
+  const [id, setId] = useState(null)
 
-  useEffect(()=>{
+  useEffect(() => {
     getList()
-  },[])
+  }, [])
   // console.log(users);
-  function getList()
-  {
-    fetch("https://jsonplaceholder.typicode.com/users").then((result)=>{
-      result.json().then((resp)=>{
+  function getList() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((result) => {
+      result.json().then((resp) => {
         // console.warn("result",resp);
         setUsers(resp)
         setName(resp[0].name)
@@ -27,22 +26,20 @@ function App() {
       })
     })
   }
-  function deleteUser(id)
-  {
-    fetch(`https://jsonplaceholder.typicode.com/users${id}`,{
-      method:'DELETE'
-    }).then((result)=>{
-      result.json().then((resp)=>{
+  function deleteUser(id) {
+    fetch(`https://jsonplaceholder.typicode.com/users${id}`, {
+      method: 'DELETE'
+    }).then((result) => {
+      result.json().then((resp) => {
         console.warn(resp);
         getList()
       })
     })
   }
 
-  function selectUser(id)
-  {
+  function selectUser(id) {
     // console.warn(users[id-1]);
-    let item = users[id-1];
+    let item = users[id - 1];
     setName(item.name);
     setUsername(item.username);
     setEmail(item.email);
@@ -50,24 +47,24 @@ function App() {
     setId(item.id);
   }
 
-  function updateUser(){
+  function updateUser() {
     console.warn(name, username, email, phone, id);
-    let item = {name, username, email, phone, id}
-    fetch(`https://jsonplaceholder.typicode.com/users${id}`,{
-      method:'PUT',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json'
+    let item = { name, username, email, phone, id }
+    fetch(`https://jsonplaceholder.typicode.com/users${id}`, {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
       },
-      body:JSON.stringify(item)
-    }).then((result)=>{
-      result.json().then((resp)=>{
+      body: JSON.stringify(item)
+    }).then((result) => {
+      result.json().then((resp) => {
         console.warn(resp);
         getList()
       })
     })
   }
-  return(
+  return (
     <div className="App">
       <h1>Get API Call</h1>
       <table border="1">
@@ -80,30 +77,30 @@ function App() {
             <td>Phone</td>
           </tr>
           {
-            users.map((item,i)=>
-            <tr key={i}>
-              <td>{users[i].id}</td>
-              <td>{users[i].name}</td>
-              <td>{users[i].username}</td>
-              <td>{users[i].email}</td>
-              <td>{users[i].phone}</td>
-              <td><button onClick={()=>deleteUser(item.id)}>Delete</button></td>
-              <td><button onClick={()=>selectUser(item.id)}>Update</button></td>
-            </tr>
+            users.map((item, i) =>
+              <tr key={i}>
+                <td>{users[i].id}</td>
+                <td>{users[i].name}</td>
+                <td>{users[i].username}</td>
+                <td>{users[i].email}</td>
+                <td>{users[i].phone}</td>
+                <td><button onClick={() => deleteUser(item.id)}>Delete</button></td>
+                <td><button onClick={() => selectUser(item.id)}>Update</button></td>
+              </tr>
             )
           }
         </tbody>
       </table>
 
       <div>
-          <input type="text" value={name} onChange={(e)=>setName(e.target.value)}/> <br/> <br/>
-          <input type="text" value={username} onChange={(e)=>setUsername(e.target.value)}/> <br/> <br/>
-          <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}/> <br/> <br/>
-          <input type="text" value={phone} onChange={(e)=>setPhone(e.target.value)}/> <br/> <br/>
-          <button onClick={updateUser}>Update User</button>
-        </div>
+        <input type="text" value={name} onChange={(e) => setName(e.target.value)} /> <br /> <br />
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} /> <br /> <br />
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} /> <br /> <br />
+        <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} /> <br /> <br />
+        <button onClick={updateUser}>Update User</button>
+      </div>
     </div>
-    
+
   );
 }
 
